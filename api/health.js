950c74eee3493c6324af6b356111789a7fd4b915
codecrafterpixel-debug <codecrafterpixel@gmail.com>
2026-07-29 22:13:15 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,10 +1,11 @@
 module.exports = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return res.status(204).end();
   }
 
-  return res.status(200).json({ status: 'ok' });
+  return res.status(200).json({ status: "ok" });
+};
