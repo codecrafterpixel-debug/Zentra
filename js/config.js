10244ac55669f40e4804122e2691ab2ebf51b7b6
codecrafterpixel-debug <codecrafterpixel@gmail.com>
2026-07-30 16:51:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,6 @@ window.ZENTRA_CONFIG = {
   adminWhatsApp: "919662781902",
   adminPhoneFormatted: "+91 96627 81902",
   apiBaseUrl: "/api",
+  supabaseUrl: "https://mndkblhxacdmgkbrmybv.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZGtibGh4YWNkbWdrYnJteWJ2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NTMxOTQyMiwiZXhwIjoyMTAwODk1NDIyfQ.y-T8R7Q2QRlou_kOmRvepnsd6M1jJRA8chdQhksJHqA"
 };
