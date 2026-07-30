9280cdf464dd38b46555438cbe955432cbb4bc84
codecrafterpixel-debug <codecrafterpixel@gmail.com>
2026-07-30 13:58:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.ZENTRA_CONFIG = {
   adminWhatsApp: "919662781902",
   adminPhoneFormatted: "+91 96627 81902",
-  apiBaseUrl: "https://zentra-clothing-store.vercel.app/api",
+  apiBaseUrl: "/api",
 };
